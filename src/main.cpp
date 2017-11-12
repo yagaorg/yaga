7803e6748972410b88e90ffa138d8efb117dfa8c
Gustorn <gustorn@gmail.com>
2017-11-12 23:32:02 +0100

    Basic CMake setup for Qt/QML and C++

---
+++ src/main.cpp
@@ -0,0 +1,18 @@
+#include <QGuiApplication>
+#include <QQmlApplicationEngine>
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+    QGuiApplication app(argc, argv);
+
+    QQmlApplicationEngine engine;
+    engine.addImportPath("qrc:///");
+    engine.addImportPath("qrc:///Components");
+    engine.addImportPath("qrc:///Pages");
+    engine.load(QUrl(QLatin1String("qrc:/Main.qml")));
+    if (engine.rootObjects().isEmpty())
+        return -1;
+
+    return app.exec();
+}
